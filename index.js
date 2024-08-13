0ec74822692b259287b2b8ac186473cc0e3e6fcd
SodiqBalogun <sbalogun157@gmail.com>
2024-08-13 18:32:03 -0500

    Properly used template literals to complete challenge 4.

--- index.js
+++ index.js
@@ -70,13 +70,17 @@ const contributionCalculate = GAMES_JSON.reduce( (acc, backer) => {
 }, 0);
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
-contributionsCard.innerHTML = contributionCalculate.toLocaleString('en-US');
+contributionsCard.innerHTML = `${contributionCalculate.toLocaleString('en-US')}`;
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
 
-// set inner HTML using template literal
+const raisedCalculate = GAMES_JSON.reduce( (acc, raised) => {
+    return acc + raised.pledged;
+}, 0);
 
+// set inner HTML using template literal
+raisedCard.innerHTML = `${raisedCalculate.toLocaleString('en-US')}`;
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
