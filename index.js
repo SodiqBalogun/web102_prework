2d3c769fac9ec665dd8e1f1a2dcdcdca6b54e0da
SodiqBalogun <sbalogun157@gmail.com>
2024-08-13 19:08:07 -0500

    Completed the third part of challenge 5.

--- index.js
+++ index.js
@@ -121,14 +121,12 @@ function filterFundedOnly() {
     addGamesToPage(funded);
 }
 
-filterFundedOnly();
-
 // show all games
 function showAllGames() {
     deleteChildElements(gamesContainer);
 
     // add all games from the JSON data to the DOM
-
+    addGamesToPage(GAMES_JSON);
 }
 
 // select each button in the "Our Games" section
